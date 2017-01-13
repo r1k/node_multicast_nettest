344421731ba82db8544b3aa9efc2f72726c8ff4a
Richard Kelsey <richard.kelsey@ericsson.com>
2017-01-13 11:55:42 +0000

    Add loops for output handling

--- proxy.js
+++ proxy.js
@@ -2,31 +2,38 @@ var HOST = '10.20.19.1';
 
 var INCOMING_MULTICAST = process.argv[2];
 var INCOMING_PORT = process.argv[3];
-
 var DESTINATION_MULTICAST = process.argv[4];
 var DESTINATION_PORT = process.argv[5];
-
 var DESTINATION_MULTICAST2 = process.argv[6];
 var DESTINATION_PORT2 = process.argv[7];
 
 var dgram = require('dgram');
 
 var incoming = dgram.createSocket({type: 'udp4', reuseAddr: true});
-var outgoing = dgram.createSocket({type: 'udp4', reuseAddr: true});
-var outgoing2 = dgram.createSocket({type: 'udp4', reuseAddr: true});
+var outgoing = []
+outgoing.push(
+    {
+        socket:            dgram.createSocket({type: 'udp4', reuseAddr: true}),
+        multicast_address: DESTINATION_MULTICAST,
+        multicast_port:    DESTINATION_PORT
+    }
+);
 
-outgoing.bind(HOST);
-outgoing.on('listening', function() {
-    outgoing.setBroadcast(true);
-    outgoing.setMulticastTTL(128);
-    outgoing.addMembership(DESTINATION_MULTICAST, HOST)
-});
+outgoing.push(
+    {
+        socket:            dgram.createSocket({type: 'udp4', reuseAddr: true}),
+        multicast_address: DESTINATION_MULTICAST2,
+        multicast_port:    DESTINATION_PORT2
+    }
+);
 
-outgoing2.bind(HOST);
-outgoing2.on('listening', function() {
-    outgoing2.setBroadcast(true);
-    outgoing2.setMulticastTTL(128);
-    outgoing2.addMembership(DESTINATION_MULTICAST2, HOST)
+outgoing.forEach(function(og){
+    og.socket.bind(HOST);
+    og.socket.on('listening', function() {
+        og.socket.setBroadcast(true);
+        og.socket.setMulticastTTL(128);
+        og.socket.addMembership(og.multicast_address, HOST);
+    });
 });
 
 incoming.on('listening', function () {
@@ -37,27 +44,19 @@ incoming.on('listening', function () {
     incoming.addMembership(INCOMING_MULTICAST, HOST);
 });
 
-
-var input_filter_pass = function() {
-    return true;
-};
-
-var output_filter_pass = function() {
-    if (Math.random() > 0.01) {
-        return true;
-    }
-    return false;
-}
-
+var input_filter_pass  = () => (true);
+var output_filter_pass = () => (Math.random() > 0.005);
 
 incoming.on('message', function (incoming_packet, remote) {
     if (input_filter_pass()) {
-        if (output_filter_pass()) {
-            outgoing.send(incoming_packet, 0, incoming_packet.length, DESTINATION_PORT, DESTINATION_MULTICAST);
-        }
-        if (output_filter_pass()) {
-            outgoing2.send(incoming_packet, 0, incoming_packet.length, DESTINATION_PORT2, DESTINATION_MULTICAST2);
-        }
+        outgoing.forEach(function(og) {
+            if (output_filter_pass()) {
+                og.socket.send(incoming_packet,
+                               0,
+                               incoming_packet.length,
+                               og.multicast_port,og.multicast_address);
+            }
+        });
     }
 });
 
