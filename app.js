3bc20c6ae421809a59d33f07c2a321f6560c736a
Kelsey <r.i.kelsey@gmail.com>
2017-01-18 22:24:35 +0000

    Add static folder

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/static'))
+
 http.listen(process.env.PORT || 5000, function() {  
   console.log('listening');
 });
