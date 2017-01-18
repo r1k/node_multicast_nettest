46d6fb0b8aafa2ae940de39c2e53dcd1e9c2cf7e
Kelsey <r.i.kelsey@gmail.com>
2017-01-18 22:51:59 +0000

    add jquery and setup basic html

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var express = require('express'); 
 var app     = express();
 var http    = require('http').Server(app);
-var io      = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/static'))
+var logger  = require('morgan');
+//var io      = require('socket.io')(http);
 
-http.listen(process.env.PORT || 5000, function() {  
+app.use(logger('dev'));
+
+var file_path = __dirname + '/static';
+app.use(express.static(file_path));
+
+http.listen(5000, function() {  
   console.log('listening');
 });
 
-io.on('connection', function(socket) {  
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
-});
+//io.on('connection', function(socket) {  
+//  console.log('a user connected');
+//  socket.on('disconnect', function() {
+//    console.log('user disconnected');
+//  });
+//});
