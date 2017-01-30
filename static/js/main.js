3c1e92729334527a370ddf875b35e45f4a109fa4
Kelsey <r.i.kelsey@gmail.com>
2017-01-30 00:18:36 +0000

    Update socket io to use url

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 var socketio;
+var server = "http://127.0.0.1"
 
 function process_config(data) {
     console.log(data);
@@ -15,7 +16,9 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     // Initialise the socketio object
-    socketio = io('http://127.0.0.1:8080');
+    server = 'http://' + window.location.hostname + ':' + window.location.port;
+    
+    socketio = io(server);
 
     // Add callback to handle message from server
     socketio.on('config', process_config);
